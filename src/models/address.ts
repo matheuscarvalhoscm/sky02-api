3ed6e61146d9de4333bdf8df65e73a08da8ce769
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-08-11 17:09:23 -0300

    feat: create model to get create an address

--- src/models/address.ts
+++ src/models/address.ts
@@ -1,9 +1,16 @@
-import fs from 'fs/promises';
 import IAddress from '../interfaces/Address';
+import { read, write } from '../utils/functions';
 
 export const getAddresses = async (): Promise<IAddress[]> => {
-  const data = await fs.readFile('src/addresses.json', 'utf8');
-  const addresses: IAddress[] = JSON.parse(data);
+  const addresses = await read();
 
   return addresses;
+};
+
+export const create = async (address: IAddress): Promise<IAddress> => {
+  const addresses = await read();
+  addresses.push(address);
+  await write(addresses);
+
+  return address;
 };
