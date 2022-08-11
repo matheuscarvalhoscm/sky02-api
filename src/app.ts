2e0d87cdcd35a776997ed0e296791348f9a2466d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-08-11 16:11:23 -0300

    feat: update `app.ts`

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import router from './routes/address';
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (_req, res) => {
-  return res.status(200).send('IT WORKED!')!
-});
+app.use('/address', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
