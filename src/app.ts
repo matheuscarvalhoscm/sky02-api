4e15de24a156cc222d9f70c3e225a62ece2c00ff
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-08-14 12:26:43 -0300

    fea: use cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+const cors = require('cors');
 import express from 'express';
 import router from './routes/address';
 
 const app = express();
 const PORT = 3001;
 
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(express.json());
 app.use('/address', router);
 
